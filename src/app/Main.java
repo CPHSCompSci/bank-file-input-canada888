3b00cacd02489949c9d0e3c10b0c65e42d983bad
matthche <matthche@014-1617-S07QZ.students.cisd.conroeisd.loc>
2019-10-10 12:48:00 -0500

    Draft 2

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,18 +5,56 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		Scanner userInput = new Scanner(System.in);
+		Scanner bankInfo = new Scanner(System.in);
 		
+		Bank newBank = new Bank();
+		int choice = 0;
 		do {
 		System.out.println("Menu");
 		System.out.println("1. Deposit");
 		System.out.println("2. Withdraw");
 		System.out.println("3. Check");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+		System.out.println("4. Create");
+		System.out.println("5. Close");
+		System.out.println("6. Transfer");
 		System.out.println("");
 		
-		} while(choice > 1);
+		System.out.println("What would you like to do?");
+		choice = userInput.nextInt();
+		
+		switch(choice)
+		{
+			case 1:
+				System.out.println("What is your account number?");
+				String number = bankInfo.next();
+				
+				System.out.println("How much would you like to deposit?");
+				String deposit = bankInfo.next();
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				System.out.println("What is your name?");
+				String name = bankInfo.nextLine();
+				int accountNo = newBank.createAccount(name);
+				
+				System.out.println("Your account number is " + accountNo);
+				break;
+			case 5:
+				
+				break;
+			case 6:
+				
+				break;
+			
+		}
+		
+		
+		} while(choice >= 1 && choice <= 6);
 		
 		//example1();
 	}
